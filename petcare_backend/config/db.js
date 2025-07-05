84cf88a4d2644e5f8e966b620b4808bc9f48382e
keyush ghimire <keyushghimire12@gmail.com>
2025-07-05 22:36:10 +0545

    Set MongoDB URI and remove unused options

--- petcare_backend/config/db.js
+++ petcare_backend/config/db.js
@@ -3,12 +3,8 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     console.log('MongoDB URI:', process.env.MONGO_URI);
-//     Connects to the MongoDB database using the connection string stored in the environment variable MONGO_URI.
-// `useNewUrlParser` and `useUnifiedTopology` are options to use the latest MongoDB drivers for better connection handling.
-    await mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
