114603371cc4cafb0910269c2a3b7a7b2dfd7b57
keyush ghimire <keyushghimire12@gmail.com>
2025-07-07 21:36:56 +0545

    Update user model and login with better security.

--- petcare_backend/models/user.js
+++ petcare_backend/models/user.js
@@ -1,38 +1,15 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-  },
-  lastname: {
-      type: String,
-      required: true,
-    },
     email: {
       type: String,
       required: true,
     },
-    // password: {
-    //   type: String,
-    //   required: true,
-    // },
-    phonenumber: {
-      type: Number,
-      required: true,
-    },
-    petname: {
+    password: {
       type: String,
       required: true,
     },
-    pettype: {
-      type: String,
-      required: true,
-    },
-    petnickname: {
-      type: String,
-      required: true,
-    },
+  
   });
 
 
--- petcare_backend/server.js
+++ petcare_backend/server.js
@@ -27,24 +27,69 @@ app.use(cookieParser());
 
 // Root Route
 app.get('/', (req, res) => {
-  res.redirect('/register.html'); 
+  res.redirect('/login-page.html'); 
 });
 
+// register page
 app.post('/create', async (req, res) => {
-  let { firstname, lastname, email, phonenumber, petname, pettype, petnickname } = req.body;
+  let { email, password, cpassword } = req.body; 
 
-  let createdUser = await userModel.create({
-    firstname,
-    lastname,
-    email,
-    phonenumber,
-    petname,
-    pettype,
-    petnickname
-  })
+  // validate password/ compare with confirm password
+  if (password !== cpassword) {
+    return res.send('Passwords do not match');
+  }
 
-res.send(createdUser);
+  try{
+    // hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt); 
+    
+    // create user
+    const createdUser = await userModel.create({
+      email,
+      password: hashedPassword
+    });
+
+    // generate token
+    let token = jwt.sign({ email }, process.env.JWT_SECRET);
+
+    // set cookie
+    res.cookie('token', token , {
+      httpOnly: true, // This cookie can only be used by the server, not by any JavaScript running in the browser.
+      maxAge: 24 * 60 * 60 * 1000 // 1 day
+    });
+
+    // send response
+
+  } catch (err) {
+    console.error(err);
+    return res.send('Server Error');
+  }
 });
+
+// Login Route
+app.post('/login', async function (req, res) {
+  let user = await userModel.findOne({ email: req.body.email });
+  if (!user) {
+    return res.send('User not found');
+  }
+  bcrypt.compare(req.body.password, user.password, function (err, result) {
+    if(result){
+       let token = jwt.sign({email: user.email}, process.env.JWT_SECRET);
+        res.cookie('token', token);
+      res.send('Login successful');
+    } 
+    else {
+      res.send('Invalid password');
+    }
+  })
+})
+
+// Logout 
+app.get("/logout", function(req, res) {
+  res.clearCookie("token", "");
+  res.redirect("/login-page.html");
+})
 
 // Connect to DB and start server
 connectDB().then(() => {
